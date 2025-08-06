e92765704f007350ed100843b0d8cfe2afeedada
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-06 22:43:11 +0530

    Explanation Added

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,5 @@
-public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class FirstProgram { // Writing the Public here doesn't affect the program
+    public static void main(String[] args) { // static means this method can be called without creating an instance of the class, void means this method doesn't return a value, string [] args means this method can accept an array of strings as arguments
+        System.out.println("Hello, World!"); // System.out.println is a method that prints the string inside the parentheses to the console
     }
 }
