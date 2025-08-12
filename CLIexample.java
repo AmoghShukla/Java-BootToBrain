7f42dca4235c8b727c2503adc848d3b49c8752c1
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-12 23:27:12 +0530

    Command Line Interface-Arguments Added

---
+++ CLIexample.java
@@ -0,0 +1,12 @@
+public class CLIexample {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Command-line arguments received:");
+            for (String item : args) { // item : args is a simpler form of for(String item in args) which means that it iterates over every element of args
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("No command-line arguments provided.");
+        }
+    }
+}
