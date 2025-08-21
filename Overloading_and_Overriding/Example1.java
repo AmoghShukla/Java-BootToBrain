f54707b0ffbfcfd7b31f90fe8ae72031c9b65815
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-21 21:49:38 +0530

    Method Overloading and Overriding

--- Overloading_and_Overriding/Example1.java
+++ Overloading_and_Overriding/Example1.java
@@ -7,7 +7,7 @@ class A{
 }
 
 class B extends A{
-    public void f1(int x, int y){
+    public void f1(int x, int y){ // Method Being Overridden
         System.out.println("Class B");
     }
 }
@@ -15,7 +15,7 @@ class B extends A{
 public class Example1 {
     public static void main(String []args){
         B obj = new B();
-        obj.f1(5);
-        obj.f1(5, 10);
+        obj.f1(5); // When one argument is passed the method from class A is called
+        obj.f1(5, 10); // When two arguments are passed the method from class B is called
     }
 }
