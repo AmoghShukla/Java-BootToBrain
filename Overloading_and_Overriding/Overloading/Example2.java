7d2d1527cdcae4477b1d07efb07a4b67aca509bf
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-21 22:13:18 +0530

    Few More Examples Added

--- Overloading_and_Overriding/Overloading/Example2.java
+++ Overloading_and_Overriding/Overloading/Example2.java
@@ -1,18 +1,20 @@
 package Overloading_and_Overriding.Overloading;
 
 class A{
-    public void f1(int x){
-        System.out.println("Class A");
-    }
+    
 }
 
 class B extends A{
-    public void f1(int x, int y){ // Method Being Overridden
-        System.out.println("Class B");
+    public void f1(int x){
+        System.out.println("Class B - 1");
+    }
+
+    public void f1(int x, int y){ // Method Being Overloaded
+        System.out.println("Class B - 2");
     }
 }
 
-public class Example1 {
+public class Example2 {
     public static void main(String []args){
         B obj = new B();
         obj.f1(5); // When one argument is passed the method from class A is called
--- Overloading_and_Overriding/Overloading/Example3.java
+++ Overloading_and_Overriding/Overloading/Example3.java
@@ -4,18 +4,20 @@ class A{
     public void f1(int x){
         System.out.println("Class A");
     }
-}
 
-class B extends A{
     public void f1(int x, int y){ // Method Being Overridden
-        System.out.println("Class B");
+        System.out.println("Class A -- 2");
     }
 }
 
-public class Example1 {
+class B extends A{
+    
+}
+
+public class Example3 {
     public static void main(String []args){
         B obj = new B();
         obj.f1(5); // When one argument is passed the method from class A is called
-        obj.f1(5, 10); // When two arguments are passed the method from class B is called
+        obj.f1(5, 10); // When two arguments are passed the method from class A --2  is called
     }
 }
