bff4c5591f2f4cf29ffe0a2e891b99efd392d59c
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-29 21:46:08 +0530

    Constructors in Inheritance Added

---
+++ Inheritance/ConstructorsInInheritance.java
@@ -0,0 +1,25 @@
+package Inheritance;
+
+class A{
+    int a ;
+    public A(){
+        System.out.println("In A's constructor");
+        a = 10;
+    }
+}
+class B extends A{
+    int b;
+    public B(){
+        // super(); // Implicit call to superclass constructor
+        System.out.println("In B's constructor");
+        b = 20;
+    }
+}
+
+public class ConstructorsInInheritance {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println("a: " + obj.a); // Accessing inherited member
+        System.out.println("b: " + obj.b); // Accessing own member
+    }
+}
