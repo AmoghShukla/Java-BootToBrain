1be65ea44e938295ae26f737af0bb7f353b17beb
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-29 21:48:25 +0530

    Constructor Chaining Added

---
+++ Inheritance/ConstructorChainning.java
@@ -0,0 +1,29 @@
+package Inheritance;
+
+class A{
+    int a ;
+    public A(){
+        System.out.println("In A's constructor");
+        a = 10;
+    }
+}
+
+class B extends A{
+    int b;
+    public B(){
+        this(4);
+        System.out.println("In B's constructor");
+    }
+    public B(int value){
+        System.out.println("In B's parameterized constructor");
+        b = value;
+    }
+}
+
+public class ConstructorChainning {
+    public static void main(String[] args) {
+        B obj1 = new B();
+        B obj2 = new B(10);
+    }
+
+}
