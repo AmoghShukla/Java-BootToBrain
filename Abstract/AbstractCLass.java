ff12d5e6896f56c9233f9950da7b3efe2644e92d
Anjali Shukla <amoghshukla548@gmail.com>
2025-08-30 16:10:57 +0530

    Abstrac Class Added

---
+++ Abstract/AbstractCLass.java
@@ -0,0 +1,16 @@
+package Abstract;
+
+abstract class A{
+
+}
+abstract class B extends A{
+    public void B(){};
+
+}
+
+public class AbstractCLass {
+    public static void main(String[] args){
+        //A o1 = new B();
+    }
+    
+}
