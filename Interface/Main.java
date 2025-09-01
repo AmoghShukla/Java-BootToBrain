7a63eb121321e7521d6be3a4b0dfd2f57ceff837
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-01 19:16:01 +0530

    Main.java

---
+++ Interface/Main.java
@@ -0,0 +1,32 @@
+package Interface;
+
+interface I1{
+    void f1();
+}
+interface I2{
+    void f2();
+}
+interface I3{
+    void f3();
+}
+class A implements I1, I2, I3{
+    public void f1(){
+        System.out.println("f1 method");
+    }
+    public void f2(){
+        System.out.println("f2 method");
+    }
+    public void f3(){
+        System.out.println("f3 method");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.f1();
+        a.f2();
+        a.f3();
+    }
+}
