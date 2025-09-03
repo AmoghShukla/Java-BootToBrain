48c94c16d5f5f8312e50a2e0a99aeafed2a2e291
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-03 18:02:23 +0530

    Arrays Added

---
+++ Array/Array.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.*;
+
+public class Array {
+    public static void main(String [] args){
+        int [] arr = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 numbers: ");
+        for(int i = 0; i<5; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i<5; i++){
+            System.out.println("Element at index "+i+" : "+arr[i]);
+            System.out.println("arr["+i+"] : "+arr[i]);
+            
+        }
+    }
+    
+}
