6481c631286e3c469a552b09c137e652bd6ab467
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-04 11:51:13 +0530

    String Methods / Upper Lower Casing Added

---
+++ String/StringMethods/UpperLower.java
@@ -0,0 +1,13 @@
+package String.StringMethods;
+
+public class UpperLower {
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        System.out.println("Original String: " + str1);
+        String Str2 = str1.toUpperCase();
+        System.out.println("Uppercase String: " + Str2);
+        String Str3 = str1.toLowerCase();
+        System.out.println("Lowercase String: " + Str3);
+    }
+    
+}
