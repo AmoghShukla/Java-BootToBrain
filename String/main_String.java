52abb1cd736c8beb9c90792f6e373668c8a6d015
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-04 11:24:46 +0530

    String in Java Added

---
+++ String/main_String.java
@@ -0,0 +1,11 @@
+package String;
+
+public class main_String {
+    public static void main(String[] args){
+        String Str1 = "Hello";
+        String Str2 = "World";
+        String Str3 = Str1 + " " + Str2;
+        System.out.println(Str3);
+    }
+
+}
