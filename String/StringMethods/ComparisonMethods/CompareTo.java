deb418c044e38d4bcd6aa72855fee5d61d9ef196
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-05 23:56:09 +0530

    Compare to in java made more readable

--- String/StringMethods/ComparisonMethods/CompareTo.java
+++ String/StringMethods/ComparisonMethods/CompareTo.java
@@ -1,9 +1,11 @@
 package String.StringMethods.ComparisonMethods;
 
 public class CompareTo {
+    // compareTo() method compares two strings lexicographically.
+    // The comparison is based on the Unicode value of each character in the strings.
     public static void main(String[] args){
-        String s1 = "apple";
-        String s2 = "Apple";
+        String s1 = "apple"; // lowercase 'a' has a higher Unicode value than uppercase 'A'
+        String s2 = "Apple"; // uppercase 'A' has a lower Unicode value than lowercase 'a'
         int result = s1.compareTo(s2);
         if (result == 0){
             System.out.println(s1 + " & " + s2 + " are same");
