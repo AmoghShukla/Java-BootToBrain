fe160975f487a5724f22a7d80f22b096ee123ce1
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-08 07:53:15 +0530

    Started Exception Handling in Java

---
+++ ExceptionHandling/Introduction.java
@@ -0,0 +1,16 @@
+package ExceptionHandling;
+
+public class Introduction {
+    public static void main(String[] args) {
+        String s1 = null;
+        System.out.println("Welcome to Exception Handling in Java!");
+        try {
+            System.out.println("Length of s1: " + s1.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught: " + e.getMessage());
+        }
+        // The .getMessage() method returns the detail message string of this throwable.
+        // It can be useful for debugging purposes.
+        System.out.println("End of the program.");
+    }
+}
