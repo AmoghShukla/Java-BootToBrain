8029228eac6714d962426b367c09e0c148ccca57
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-17 21:46:37 +0530

    Inner/Nested Class in Java Added

---
+++ InnerClass.java
@@ -0,0 +1,20 @@
+class Outer{
+    static class Inner{
+        void display(){
+            System.out.println("Inside Inner method");
+        }
+
+    }
+}
+
+
+public class InnerClass {
+    public static void main(String[] args){
+        Outer.Inner o1 = new Outer.Inner();
+        o1.display();
+    }
+    
+
+
+    
+}
