5c3fab2bd2e924a5be6200223750ca398605a76b
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-18 22:58:48 +0530

    Generic Methods Expanded

--- Generics/GenericMethod.java
+++ Generics/GenericMethod.java
@@ -1,7 +1,9 @@
 package Generics;
 
 public class GenericMethod {
-
+    // Using Generics to create a single method that can handle different types of arrays
+    // The Syntax for defining a generic method includes a type parameter (like <X>) before the return type of the method.
+    // Here, X is a placeholder for the actual type that will be specified when the method is called.
     public <X> void printArray(X[] array){ 
         for(int i=0; i<array.length; i++){
             System.out.println(array[i]);
@@ -10,8 +12,8 @@ public class GenericMethod {
 
     public static void main(String[] args){
         GenericMethod gm = new GenericMethod();
-        String[] strArray = new String[]{"India", "USA", "UK" , "Canada"};
-        Integer[] intArray = new Integer[]{1, 2, 3, 4, 5};
+        String[] strArray = new String[]{"India", "USA", "UK" , "Canada"}; // Array of Strings
+        Integer[] intArray = new Integer[]{1, 2, 3, 4, 5}; // Array of Integers
         gm.printArray(strArray);
         gm.printArray(intArray);
     }
