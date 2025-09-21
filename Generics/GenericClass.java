aa5507c21429320cbcfc344df899ec30c31e7dc2
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-21 18:56:14 +0530

    Generic Class.java Added

--- Generics/GenericClass.java
+++ Generics/GenericClass.java
@@ -1,3 +1,7 @@
+// The Main Difference Between Generic Class and Generic Method is that:
+// A Generic Class is a class that can work with different data types, allowing you to create objects of the class with various types without needing to write multiple versions of the class.
+// A Generic Method is a method that can operate on different data types, allowing you to write a single method that can handle various types without needing to overload the method for each type.
+
 package Generics;
 
 class MyData <T>{
