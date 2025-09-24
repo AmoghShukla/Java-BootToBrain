2a7a2476244ed3da13d7e24db3319b503c5211ee
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-24 23:13:56 +0530

    Array List Added

--- Collection/List/ArrayList1.java
+++ Collection/List/ArrayList1.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ArrayList1 {
+    public static void main(String[] args){
+        ArrayList nameList = new ArrayList(4);
+        nameList.add("Amogh");
+        nameList.add("Shukla");
+        nameList.add("Aman");
+        nameList.add("Kumar");
+    }
     
 }
