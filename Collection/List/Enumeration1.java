5dd5f555b926a509cc220ee084e0c13e9879c374
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-26 18:28:21 +0530

    Enumertaion Added

---
+++ Collection/List/Enumeration1.java
@@ -0,0 +1,18 @@
+package Collection.List;
+import java.util.*;
+
+public class Enumeration1 {
+    public static void main(String[] args){
+        Vector v = new Vector();
+        v.addElement("One");
+        v.addElement("Two");
+        v.addElement("Three");
+
+        Enumeration e = v.elements(); 
+        while(e.hasMoreElements()){
+            String s = (String)e.nextElement();
+            System.out.println(s);
+        }
+    }
+    
+}
