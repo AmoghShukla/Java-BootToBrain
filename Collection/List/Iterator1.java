b940903bae1428899db99ed2a05364c44b683ba1
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-26 19:15:08 +0530

    Iterator Added

---
+++ Collection/List/Iterator1.java
@@ -0,0 +1,21 @@
+package Collection.List;
+import java.util.*;
+
+public class Iterator1 {
+    public static void main(String[] args){
+        ArrayList al = new ArrayList();
+        for(int i=0; i<=10; i++){
+            al.add(i);
+        }
+        Iterator it = al.iterator();
+        while(it.hasNext()){
+            Integer I = (Integer)it.next();
+            if(I > 3 && I < 8){
+                it.remove();
+            }
+            System.out.println(I);
+        } 
+        System.out.println(al);
+    }
+    
+}
