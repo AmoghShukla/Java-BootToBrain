6a694bf72bfa29c99dc32b98a99edbf39e09f28c
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-27 21:50:52 +0530

    HashSet1 --No Arguments Added

---
+++ Collection/Collection_Set/HashSet1.java
@@ -0,0 +1,16 @@
+package Collection.Collection_Set;
+import java.util.*;
+
+public class HashSet1 {
+    public static void main(String[] args){
+        HashSet H1 = new HashSet();
+        H1.add("Amogh1");
+        H1.add("Amogh2");
+        H1.add("Amogh3");
+        H1.add("Amogh4");
+        H1.add("Amogh5");
+        H1.add("Amogh6");
+        System.out.println(H1);
+        
+    }    
+}
