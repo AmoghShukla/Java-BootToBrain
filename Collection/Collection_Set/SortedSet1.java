a3c7c92f5f7116f1168c481e1fa4adb51195af9c
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-28 22:33:30 +0530

    Sorted Set Added

---
+++ Collection/Collection_Set/SortedSet1.java
@@ -0,0 +1,15 @@
+package Collection.Collection_Set;
+import java.util.*;
+
+
+public class SortedSet1 {
+    public static void main(String[] args) {
+        SortedSet sortedSet = new TreeSet();
+        sortedSet.add("Banana");
+        sortedSet.add("Apple");
+        sortedSet.add("Mango");
+        sortedSet.add("Orange");
+
+        System.out.println("Sorted Set: " + sortedSet);
+    }
+}
