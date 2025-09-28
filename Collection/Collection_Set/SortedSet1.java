a489254a377514f78be764bbaaceb3eece53d122
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-28 22:34:07 +0530

    Sorted Set Added

--- Collection/Collection_Set/SortedSet1.java
+++ Collection/Collection_Set/SortedSet1.java
@@ -11,5 +11,6 @@ public class SortedSet1 {
         sortedSet.add("Orange");
 
         System.out.println("Sorted Set: " + sortedSet);
+        System.out.println("First Element: " + sortedSet.first());
     }
 }
