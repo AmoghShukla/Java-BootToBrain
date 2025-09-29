575da7dbc0b396aeb8931fe1dde87b1cc51a4e5a
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-29 08:36:41 +0530

    NavigableSet Added

---
+++ Collection/Collection_Set/NavigableSet1.java
@@ -0,0 +1,15 @@
+package Collection.Collection_Set;
+import java.util.*;
+
+public class NavigableSet1 {
+    public static void main(String[] args) {
+        NavigableSet navigableSet = new TreeSet();
+        navigableSet.add("Banana");
+        navigableSet.add("Apple");
+        navigableSet.add("Mango");
+        navigableSet.add("Orange");
+
+       
+    }
+    
+}
