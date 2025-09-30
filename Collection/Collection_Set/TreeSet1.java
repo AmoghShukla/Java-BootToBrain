14dc4a9e77504365559e5afe163146899a7f3efc
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-30 20:47:32 +0530

    Extended TreeSet1

---
+++ Collection/Collection_Set/TreeSet1.java
@@ -0,0 +1,10 @@
+package Collection.Collection_Set;
+import java.util.*;
+
+public class TreeSet1 {
+    public static void main(String [] args){
+        TreeSet ts = new TreeSet();
+
+    }
+    
+}
