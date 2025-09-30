97ce5daebc6b89c5d98ef15d5392873c1be482da
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-30 20:48:06 +0530

    Extended TreeSet1

--- Collection/Collection_Set/TreeSet1.java
+++ Collection/Collection_Set/TreeSet1.java
@@ -4,7 +4,12 @@ import java.util.*;
 public class TreeSet1 {
     public static void main(String [] args){
         TreeSet ts = new TreeSet();
-
+        ts.add("A");
+        ts.add("B");
+        ts.add("C");
+        ts.add("D");
+        ts.add("E");
+        System.out.println(ts);
     }
     
 }
