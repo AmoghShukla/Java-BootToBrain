6986fa6b5d31a3bef0e841fe2deda1fbb14a44ba
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-02 23:27:15 +0530

    TreeSet2 Added

--- Collection/Collection_Set/TreeSet2.java
+++ Collection/Collection_Set/TreeSet2.java
@@ -2,5 +2,14 @@ package Collection.Collection_Set;
 import java.util.*;
 
 public class TreeSet2 {
+    public static void main(String [] args){
+        TreeSet ts = new TreeSet();
+        ts.add("A");
+        ts.add("B");
+        ts.add("C");
+        ts.add("D");
+        ts.add("E");
+        System.out.println(ts);
+    }
     
 }
