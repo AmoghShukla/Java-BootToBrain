183cedc8b74411ef28063e693d6bc20b003c70a1
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-03 22:27:39 +0530

    TreeSet Example

---
+++ Collection/Collection_Set/TreesetExample.java
@@ -0,0 +1,15 @@
+package Collection.Collection_Set;
+import java.util.*;
+
+class Book{
+    private int price;
+    private String name;
+    public Book(int price, String name){
+        this.price = price;
+        this.name = name;
+    }
+}
+
+public class TreesetExample {
+    
+}
