418c11d82f010e8c756bc13f4bf27afc652376e4
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-04 23:25:18 +0530

    TreeSet2 Commented

--- Collection/Collection_Set/TreeSet2.java
+++ Collection/Collection_Set/TreeSet2.java
@@ -2,6 +2,7 @@ package Collection.Collection_Set;
 import java.util.*;
 
 public class TreeSet2 {
+    // Main Method
     public static void main(String [] args){
         TreeSet ts = new TreeSet();
         ts.add(10);
