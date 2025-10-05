9850e55733e47a7573ba142b3cf10a68c33ce625
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-05 19:03:06 +0530

    PriorityQueue Added

---
+++ Collection/Collection_Set/PriorityQueue1.java
@@ -0,0 +1,16 @@
+package Collection.Collection_Set;
+import java.util.*;
+
+public class PriorityQueue1 {
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        pq.offer("Amogh1");
+        pq.offer("Amogh2");
+        pq.offer("Amogh3");
+        pq.offer("Amogh4");
+        pq.offer("Amogh5");
+        pq.offer("Amogh6");
+        System.out.println(pq);
+    }
+    
+}
