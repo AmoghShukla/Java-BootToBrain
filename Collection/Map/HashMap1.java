b46eb5e751d403a7c8c8ca4c0658a31e4e4beae2
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-06 23:49:45 +0530

    HashMap Expanded

--- Collection/Map/HashMap1.java
+++ Collection/Map/HashMap1.java
@@ -1,13 +1,14 @@
 package Collection.Map;
-import java.utils.*;
+import java.util.*;
 
-public class HashMap1{
-    public static void main(String[] args){
-        HashMap hm = new HashMap();
-        hm.put(1,"Amogh");
-        hm.put(2,"Shukla");    
-        hm.put(4,"Anjali");
-        hm.put(3,"Shukla");
+public class HashMap1 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hm = new HashMap<>();
+        hm.put(1, "Amogh");
+        hm.put(2, "Shukla");
+        hm.put(4, "Anjali");
+        hm.put(3, "Shukla");
+
         System.out.println(hm);
     }
 }
