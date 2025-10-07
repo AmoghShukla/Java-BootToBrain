c50a25e08123c15346a7d17375ec5127737bd7ef
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-08 00:12:07 +0530

    Binary Search Method Implemented in Collections CLass

--- Collection/CollectionsClass/Example1.java
+++ Collection/CollectionsClass/Example1.java
@@ -4,12 +4,14 @@ import java.util.*;
 public class Example1 {
     public static void main(String[] args) {
         Vector v1 = new Vector();
-        v1.add("10");
-        v1.add("20");
-        v1.add("15");
-        v1.add("11");
+        v1.add(10);
+        v1.add(20);
+        v1.add(15);
+        v1.add(11);
         System.out.println(v1);
         Collections.sort(v1);
+        int x = Collections.binarySearch(v1, 15);
         System.out.println(v1);
+        System.out.println(x);
     }
 }
