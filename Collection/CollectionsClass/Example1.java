2c01c0871e9db3ed6dd553aac1118683369f3cf7
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-07 23:54:10 +0530

    Collections Class Added

---
+++ Collection/CollectionsClass/Example1.java
@@ -0,0 +1,13 @@
+package Collection.CollectionsClass;
+import java.util.*;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Vector v1 = new Vector();
+        v1.add("10");
+        v1.add("20");
+        v1.add("15");
+        v1.add("5");
+        System.out.println(v1);
+    }
+}
