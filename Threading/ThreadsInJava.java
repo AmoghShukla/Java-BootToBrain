a58805887e40ad1ea3b0a8ac958ae5cb587c6c8a
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-09 23:43:32 +0530

    Threads Using Runnable Interface Expanded

---
+++ Threading/ThreadsInJava.java
@@ -0,0 +1,17 @@
+package Threading;
+import java.lang.Runnable;
+
+public class ThreadsInJava {
+
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread is running");
+            }
+        };
+
+        Thread thread = new Thread(task);
+        thread.start();
+    }
+}
--- Threading/ThreadsUsingRunnableInterface.java
+++ Threading/ThreadsUsingRunnableInterface.java
@@ -1,20 +1,27 @@
 package Threading;
 import java.util.*;
 
-class A implements Runnable{
-    public void run(){
-        int i;
-        for(int i = 0; i<10;i++){
+class A implements Runnable {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Thread A :" + i);
         }
     }
-
 }
 
-class B implements Runnable{
-
+class B implements Runnable {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread B :" + i);
+        }
+    }
 }
 
 public class ThreadsUsingRunnableInterface {
-    
+    public static void main(String[] args) {
+        Thread T1 = new Thread(new A());
+        Thread T2 = new Thread(new B());
+        T1.start();
+        T2.start();
+    }
 }
