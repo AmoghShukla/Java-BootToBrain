e4c3b5419b7b3f2b5ad30330734ca4e28d8705a5
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-14 23:59:42 +0530

    Building the Threading Example

--- ThreadingInJava/ThreadingExample.java
+++ ThreadingInJava/ThreadingExample.java
@@ -41,5 +41,14 @@ class Customer implements Runnable{
 
 
 public class ThreadingExample {
+    public static void main(String[] args){
+        Account a1 = new Account(1000);
+        Customer c1 = new Customer(a1);
+        Thread t1 = new Thread(c1);
+        Customer c2 = new Customer(a1);
+        Thread t2 = new Thread(c2);
+
+        
+    }
     
 }
