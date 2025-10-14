1b5164eeaf5fd80b5b3cbf2aa1cd9e600312307c
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-14 23:50:25 +0530

    Building the Threading Example

--- ThreadingInJava/ThreadingExample.java
+++ ThreadingInJava/ThreadingExample.java
@@ -6,8 +6,26 @@ class Account{
     public Account(int bal){
         this.bal=bal;
     }
-    
+    public boolean hasSufficientBalance(int amt){
+        if(amt < bal){
+            return(true);
+        } else {
+            return(false);
+        }
+    }
+    public void withdraw(int amt){
+        if (hasSufficientBalance(amt)){
+            bal = bal - amt;
+            System.out.println("Withdrawn: " + amt);
+            System.out.println("Remaining Balance: " + bal);
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
 }
+
+
 
 public class ThreadingExample {
     
