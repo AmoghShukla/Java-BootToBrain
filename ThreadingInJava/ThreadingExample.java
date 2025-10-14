96d3450104986145f7c57446a6d7f37784874740
Amogh Shukla <amoghshukla548@gmail.com>
2025-10-14 23:43:07 +0530

    Building the Threading Example

---
+++ ThreadingInJava/ThreadingExample.java
@@ -0,0 +1,14 @@
+package ThreadingInJava;
+import java.util.Scanner;
+
+class Account{
+    private int bal;
+    public Account(int bal){
+        this.bal=bal;
+    }
+    
+}
+
+public class ThreadingExample {
+    
+}
