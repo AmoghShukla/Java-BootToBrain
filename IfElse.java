d27adb3c756516e1b8201d5949f3497f6fc7237b
Amogh Shukla <amoghshukla548@gmail.com>
2025-11-12 23:48:33 +0530

    Additions to the IfElse Program

--- IfElse.java
+++ IfElse.java
@@ -5,6 +5,7 @@ public class IfElse {
         int presetPIN = 1234;
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter your PIN: ");
         int newPIN = sc.nextInt();
 
         if (presetPIN == newPIN){
