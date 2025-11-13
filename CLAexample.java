ef2a56087e56a6de484fe57812376a6d8275f81b
Amogh Shukla <amoghshukla548@gmail.com>
2025-11-13 23:48:07 +0530

    Updated

--- CLAexample.java
+++ CLAexample.java
@@ -5,6 +5,7 @@ public class CLAexample {
         for( String item : args){
             s += Integer.parseInt(item); // Convert each command-line argument to an integer and add it to the sum
         }
+        // Print the sum of the command-line arguments
         System.out.println("Sum of command-line arguments: " + s);
     }
     
